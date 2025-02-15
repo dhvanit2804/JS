b8398ec903f6cb59e7f3a8054ae231f827999318
dhvanit2804 <dhvanitparate123@gmail.com>
2025-02-15 11:50:02 +0530

    js

---
+++ script.js
@@ -0,0 +1,47 @@
+// for (let i= 1; i<=100000; i++) {
+//     console.log("Hello World");
+    
+// }
+
+// let sum = 0;
+// let n = 7;
+// for (let i=1; i<=n; i++){
+//     sum = sum + i;
+// }
+// console.log("Sum = " + sum);
+// console.log("Loop Completed");
+
+// let i = 1;
+// while(i<=555){
+//     console.log("DHVANIT PARATE");
+//     i++;
+// }
+
+// let i = 1;
+// do {
+//     console.log("DHVANIT PARATE");
+//     i++;
+// } while (i<=10);
+
+
+//for of loop
+// let str = "ApnaCollege";
+
+// for(let i of str){
+//     console.log("i =", i);
+    
+// }
+
+//for in loop
+
+let student = {
+    name: "Dhvanit Parate",
+    age: 20,
+    cgpa: 8.5,
+    isPass: true,
+};
+
+for(let key in student){
+    console.log("key = ", key, "value = ", student[key]);
+    
+}
