b39bca6eae12b4a6c2acf388f058247283aa91d4
dhvanit2804 <dhvanitparate123@gmail.com>
2025-02-27 12:00:11 +0530

    js

---
+++ strings.js
@@ -0,0 +1,13 @@
+let obj = {
+    item: "pen",
+    price: 10,
+};
+
+let Output = `the cost of ${obj.item} is ${obj.price} rupees`;
+console.log(Output);
+
+
+// Template Literals
+
+let SpecialString = `This is a template literals`;
+console.log(typeof SpecialString);
