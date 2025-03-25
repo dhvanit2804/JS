540f2e5c39f36e2d55f8019d8725fe88a162214a
dhvanit2804 <dhvanitparate123@gmail.com>
2025-03-25 15:44:16 +0530

    js

--- dom.js
+++ dom.js
@@ -1,8 +1,8 @@
 // What is DOM ?
 // When a Web Page is Loaded the browser creates a DOCUMENT OBJECT MODEL(DOM) Of the Page.
-// let heading = document.getElementById("heading");
-// console.dir(heading);
+let button = document.getElementById("myId");
+console.dir(button);
 
-let headings = document.getElementsByClassName("heading-class");
+let headings = document.getElementsByClassName("myClass");
 console.dir(headings);
 console.log(headings);
