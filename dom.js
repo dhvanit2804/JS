5a805323accdb11b44a0c97f43e2898b6f2b246f
dhvanit2804 <dhvanitparate123@gmail.com>
2025-03-25 10:11:31 +0530

    js

--- dom.js
+++ dom.js
@@ -1,4 +1,8 @@
-console.log("hello");
-window.console.log("hello2");
 // What is DOM ?
 // When a Web Page is Loaded the browser creates a DOCUMENT OBJECT MODEL(DOM) Of the Page.
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+let headings = document.getElementsByClassName("heading-class");
+console.dir(headings);
+console.log(headings);
