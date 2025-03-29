e4eaf75dec1fa13d168205d42e82d421aace2ada
dhvanit2804 <dhvanitparate123@gmail.com>
2025-03-29 16:12:46 +0530

    js

--- dom2.js
+++ dom2.js
@@ -1,8 +1,4 @@
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("class", "newClass"));
+
 let div = document.querySelector("div");
-console.log(div);
-
-let id = div.getAttribute("id");
-console.log(id);
-
-let name = div.getAttribute("name");
-console.log(name);
