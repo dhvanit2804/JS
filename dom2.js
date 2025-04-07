c4f3425ac41c821801ec29a2644f43fe33f720c0
dhvanit2804 <dhvanitparate123@gmail.com>
2025-04-07 16:58:10 +0530

    js

--- dom2.js
+++ dom2.js
@@ -1,7 +1,17 @@
 // let para = document.querySelector("p");
 // console.log(para.setAttribute("class", "newClass"));
 
-let div = document.querySelector("div");
-div.style.backgroundColor = "green";
+// let div = document.querySelector("div");
+// div.style.backgroundColor = "green";
+// // div.style.visibility = "hidden";
 
-div.style.fontFamily= "Arial";
+// div.style.fontFamily= "Arial";
+
+// div.innerText = "DHVANIT";
+
+let newBtn = document.createElement("button");
+newBtn.innerText = "Click Me!";
+console.log(newBtn);
+
+let div = document.querySelector("div");
+div.append(newBtn);
