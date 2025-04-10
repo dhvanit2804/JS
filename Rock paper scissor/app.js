9f286a37d99ffc9d29fd719b00292a842dfa95a5
dhvanit2804 <dhvanitparate123@gmail.com>
2025-04-10 11:50:23 +0530

    js

--- Rock paper scissor/app.js
+++ Rock paper scissor/app.js
@@ -3,9 +3,52 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
+
+const drawGame = () => {
+  console.log("game was draw.");
+};
+
+const showWinner = (userWin) => {
+  if (userWin) {
+    console.log("you win!");
+  } else {
+    console.log("you lose!");
+  }
+};
+
+const playGame = (userChoice) => {
+  console.log("user choice =", userChoice);
+  // Generate Computer choice
+  const compChoice = genCompChoice();
+  console.log("comp choice =", compChoice);
+
+  if (userChoice === compChoice) {
+    // Draw Game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      // scissors, paper
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      // rock, scissors
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      // rock, paper
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin);
+  }
+};
+
 choices.forEach((choice) => {
-    choice.addEventListener("click", () => {
-        const userChoiceId = choice.getAttribute("id");
-        console.log("choice was clicked", userChoiceId);
-    })
-})
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+  });
+});
