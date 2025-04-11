9aae605f9e4748de4ba2d4a6af8ba26108544eed
dhvanit2804 <dhvanitparate123@gmail.com>
2025-04-11 10:52:46 +0530

    js

--- class.js
+++ class.js
@@ -3,6 +3,21 @@ const student = {
   age: 20,
   marks: 90,
   printMarks: function () {
-    console.log("marks: ", marks);
+    console.log("marks: ", this.marks);
   },
 };
+
+const employee = {
+  calcTax () {
+    console.log("tax rate is 10%");
+  }
+}
+
+const karanArjun = {
+  salary: 50000,
+  calcTax () {
+    console.log("tax rate is 20%");
+  }
+}
+
+karanArjun.__proto__ = employee;
