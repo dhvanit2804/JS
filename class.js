e67131796f815e64a65927066b39bf2f64257b92
dhvanit2804 <dhvanitparate123@gmail.com>
2025-04-11 11:43:57 +0530

    js

--- class.js
+++ class.js
@@ -8,16 +8,38 @@ const student = {
 };
 
 const employee = {
-  calcTax () {
+  calcTax() {
     console.log("tax rate is 10%");
-  }
-}
+  },
+};
 
 const karanArjun = {
   salary: 50000,
-  calcTax () {
+  calcTax() {
     console.log("tax rate is 20%");
+  },
+};
+
+karanArjun.__proto__ = employee;
+
+// Classes in JS
+// Classes are a template for creating objects. They encapsulate data with code to work on that data.
+// Classes in JS are built on prototypes but also have some syntax and semantics that are unique to classes.
+
+
+class ToyotaCar {
+  start() {
+    console.log("Car started");
+  }
+
+  stop() {
+    console.log("Car stopped");
+  }
+
+  setBrand(brand) {
+    this.brandName = brand;
   }
 }
 
-karanArjun.__proto__ = employee;
+let fortuner = new ToyotaCar();
+fortuner.setBrand("FORTUNER");
