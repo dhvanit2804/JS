a49373f6b3b4c93cc2b886510845995009d180cd
dhvanit2804 <dhvanitparate123@gmail.com>
2025-05-01 17:21:07 +0530

    js

---
+++ callback.js
@@ -0,0 +1,21 @@
+console.log("callbackjs");
+console.log("DHVANIT PARATE");
+
+setTimeout(() => {
+  console.log("I am a inside a setTimeout Function");
+}, 3000);
+
+console.log("The End");
+
+const callback = (arg) => {
+    console.log(arg);
+}
+
+const loadScript = (src, callback) => {
+  let sc = document.createElement("script");
+  sc.src = src;
+  sc.onload = callback("DHVANIT PARATE");
+  document.head.append(sc);
+};
+
+loadScript("https://code.jquery.com/jquery-3.6.0.min.js", callback());
