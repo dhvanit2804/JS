fcc4e0bae5b571435e0e92bb52b580992a9cde99
dhvanit2804 <dhvanitparate123@gmail.com>
2025-05-02 17:45:54 +0530

    js

---
+++ promises.js
@@ -0,0 +1,11 @@
+// Promises is for 'eventual' completion of task. It is an Object in js.
+// It is a Solution to the problem of 'callback hell'.
+
+let promise = new Promise((resolve, reject) => {
+  console.log("Promise is Created...");
+  reject("Some Error");
+});
+
+function getData(dataId, getNextData) {
+  return new Promise((resolve, reject) => {});
+}
