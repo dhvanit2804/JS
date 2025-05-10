5f30f1b8bc939cb0fb19b93510015f441ad19f2e
dhvanit2804 <dhvanitparate123@gmail.com>
2025-05-10 12:15:59 +0530

    js

--- api.js
+++ api.js
@@ -1,1 +1,12 @@
-console.log("hello");
+fetch("https://jsonplaceholder.typicode.com/users",{
+    method: "GET"
+})
+.then((res)=> {
+    return res.json()
+})
+.then((data)=>{
+    console.log(data)
+    for (let user of data) {
+        console.log(user.username)
+    }
+})
