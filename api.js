b64ee95cf57d7c08ec3d7cb0c3dec066cf5072bf
dhvanit2804 <dhvanitparate123@gmail.com>
2025-05-12 10:59:05 +0530

    js

--- api.js
+++ api.js
@@ -1,9 +1,13 @@
 const URL = "https://681f1721c1c291fa6635eff8.mockapi.io/practice";
+const para = document.getElementById("para");
+const btn = document.querySelector("button");
 
 const getData = async () => {
+    console.log("Fetching data...");
     let res = await fetch(URL);
     console.log(res);
     let data = await res.json();
-    console.log(data[1].name)
+    para.innerHTML = data[1].name;
 }
-getData()
+
+btn.addEventListener("click", getData);
