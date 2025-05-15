6a102707045cf8a8f2712329e6128e0d8cb87c93
dhvanit2804 <dhvanitparate123@gmail.com>
2025-05-15 14:51:47 +0530

    js

---
+++ loops_que.js
@@ -0,0 +1,33 @@
+// 1. Write a for loop that prints the numbers from 1 to 50 to the console.
+// for (let i = 1; i < 51; i++) {
+//     console.log(i);
+// }
+
+// 2. use a while loop to sum the numbers from 1 to 10
+// let a = 1;
+// let sum = 0;
+// while (a < 11) {
+//   sum = sum + a;
+//   a++;
+// }
+// console.log(sum);
+
+// 3. Create a for of loop to print each character of javascript in uppercase
+// let str = "Javascript";
+// for (let char of str) {
+//     console.log(char.toUpperCase());
+// }
+
+// 4. Write a for loop that prints the even numbers from 1 to 20
+// for (let i = 1; i < 21; i++) {
+//   if (i % 2 === 0) {
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+let a = 5;
+do {
+    console.log(a);
+    a--;
+} while (a>0);
