e821e6002376f8749bbad2af31c0dd6e3ac03086
dhvanit2804 <dhvanitparate123@gmail.com>
2025-05-30 11:08:44 +0530

    js

--- array_que.js
+++ array_que.js
@@ -1,11 +1,38 @@
-let city = ["Surat", "Ahmedabad", "Kolkata", "Delhi"]
+let city = ["Surat", "Ahmedabad", "Kolkata", "Delhi"];
 
-for(let cities of city){
-    console.log(cities.toUpperCase())
-}
+// for(let cities of city){
+//     console.log(cities.toUpperCase())
+// }
 
 const btn = document.getElementById("btn");
 
-btn.addEventListener("click",()=>{
-    console.log("Button Is Clicked")
-})
+btn.addEventListener("click", () => {
+  console.log("Button Is Clicked");
+});
+
+let a = 0;
+function person() {
+  if (a > 5) {
+    console.log("A is Match");
+  } else {
+    console.log("A is Not match");
+    return;
+  }
+}
+
+
+const plus = () =>{
+    let b = 10;
+    for (let i=1; i<b; i++){
+        console.log(i)
+    }
+}
+// plus()
+
+function getPromise (){
+    return new Promise((resolve, reject)=>{
+        console.log("Promise is Created...");
+        // resolve("Success..")
+        reject("Error...")
+    })
+}
